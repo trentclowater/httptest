655047dc85a80dd35425dab172d9aaf366d14d8f
Trent Clowater <trentclowater@gmail.com>
2022-02-14 14:11:07 +0800

    Add sample http request.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'httptest';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const url = 'https://test.apollo.bio:10443/assaya';
+
+    const options = {
+      headers: new HttpHeaders({
+        contentType:  'application/json'
+      })
+    };
+
+    const body = { action: 'registrate', token : '' };
+
+    this.http.post<any>(url, body, options).subscribe({
+      next: (success) => {
+        console.log('SUCCESS: ', success);
+      },
+      error: (error) => {
+        console.log('ERROR: ', error);
+      }
+    })
+  }
 }
